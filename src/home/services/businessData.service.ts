ac10f502651450beb851f147c095c4e148042090
khaoulaarfaouii <khaoula.arfaoui@contentsquare.com>
2022-04-07 11:22:41 +0200

    feat(BusinessData): compute service fees based on zip & compute negotiation margin

--- src/home/services/businessData.service.ts
+++ src/home/services/businessData.service.ts
@@ -17,19 +17,59 @@ export default class BusinessDataService {
     homeUuid: string,
     initialOfferPrice: number,
     finalOfferPrice: number,
-    targetSalePrice: number
+    targetSalePrice: number,
+    negociationMargin:number,
+    serviceFees:number,
   ): Promise<BusinessData> {
-    // TODO : write business data logic to compute :
-    //  - serviceFees (see README)
-    //  - negociation margin (see README)
+    
+     function computeNegotiationMargin(targetSalePrice: number, finalOfferPrice: number,maxNegociationMargin=0.07){
+     negociationMargin = Math.min((targetSalePrice/finalOfferPrice)-1 ,maxNegociationMargin)
+    }
+     
+  function computeServiceFees(finalOfferPrice:number, zipCode:number){
+       if (zipCode === 95) {
+        serviceFees = computeLilleServiceFees(finalOfferPrice);
+       }
+       else if([75,92,93,94].includes(zipCode)) {
+        serviceFees = computeParisServiceFees(finalOfferPrice);
+       }
+       else if (zipCode === 44 ) {
+        serviceFees = computeNantesServiceFees(finalOfferPrice);
+       }
+   }
+     function computeLilleServiceFees(finalOfferPrice: number):number{ 
+      if(finalOfferPrice<100000)  return 15000;
+      else if(100000<=finalOfferPrice && finalOfferPrice<145000)  return 19000;
+      else if (145000<=finalOfferPrice && finalOfferPrice<200000) return 20000;
+      else if (200000<=finalOfferPrice && finalOfferPrice<400000) return finalOfferPrice*0.1;
+      else if (4000000<=finalOfferPrice && finalOfferPrice<650000) return finalOfferPrice*0.08;
+      else  return  finalOfferPrice*0.3;
+    }
+    function computeParisServiceFees(finalOfferPrice: number):number{ 
+      if(finalOfferPrice<100000)  return 20000;
+        else if(100000<=finalOfferPrice && finalOfferPrice<145000)  return 22000;
+        else if (145000<=finalOfferPrice && finalOfferPrice<200000) return 23000;
+        else if (200000<=finalOfferPrice && finalOfferPrice<400000) return finalOfferPrice*0.11;
+        else if (4000000<=finalOfferPrice && finalOfferPrice<650000) return finalOfferPrice*0.08;
+        else  return  finalOfferPrice*0.1;
+    }
 
+     function computeNantesServiceFees(finalOfferPrice: number):number {
+      if(finalOfferPrice<100000)  return 20000;
+      else if(100000<=finalOfferPrice && finalOfferPrice<145000)  return 22000;
+      else if (145000<=finalOfferPrice && finalOfferPrice<200000) return 23000;
+      else if (200000<=finalOfferPrice && finalOfferPrice<400000) return finalOfferPrice*0.11;
+      else if (4000000<=finalOfferPrice && finalOfferPrice<650000) return finalOfferPrice*0.08;
+      else  return  finalOfferPrice*0.099;
+    }
+     
     const businessData = await this.createBusinessData({
       homeUuid,
       initialOfferPrice,
       finalOfferPrice,
       targetSalePrice,
-      // serviceFees
-      // negociationMargin
+      serviceFees,
+      negociationMargin
     });
     await this.homeService.updateHome(homeUuid, {
       businessDataUuid: businessData.uuid,
